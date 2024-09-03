d9059c5002ba40dca90aa536ce3c55827b9af0c9
unknown <willsonwill97@gmail.com>
2024-09-02 23:07:59 -0300

    versao inicial

---
+++ index.js
@@ -0,0 +1,17 @@
+var setadireita = window.document.getElementById("setadireita")
+var leonardo = window.document.getElementById("leonardo")
+var samatha = window.document.getElementById("samatha")
+var bruna = window.document.getElementById("bruna")
+var setaesquerda = window.document.getElementById("setaesquerda")
+
+function rolarparadireita(){
+    leonardo.style="display:none"
+    bruna.style=" display:flex"
+    setadireita.style ="display:none"
+    setaesquerda.style ="display:flex "}
+
+     function rolarparaesquerda(){
+        leonardo.style="display:flex"
+        bruna.style="display:none"
+        setadireita.style="dispaly:flex"
+        setaesquerda.style ="display:none "}
